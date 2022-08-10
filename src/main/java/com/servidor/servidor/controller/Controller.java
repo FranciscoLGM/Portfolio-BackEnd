11c9328f24e8044de5c8f5cf8e2dcf05867e9177
Francisco Javier López García Mayorga <javier6282@hotmail.com>
2022-08-10 20:33:53 -0300

    [UPDATE]the update method is implemented

--- src/main/java/com/servidor/servidor/controller/Controller.java
+++ src/main/java/com/servidor/servidor/controller/Controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,11 @@ public class Controller {
     @DeleteMapping("/delete/{id}")
     public void deletePerson(@PathVariable Long id) {
         personService.deletePerson(id);
+    }
+
+    @PutMapping("/update/person")
+    public void updatePeson(@RequestBody Persona person) {
+        personService.updatePerson(person);
     }
 
 }
