20f00607072737a2caaab5f0214b0d4ce13facbf
Francisco Javier López García Mayorga <javier6282@hotmail.com>
2022-08-15 22:41:21 -0300

    [ADD] CORS

--- src/main/java/com/servidor/servidor/controller/ControllerEducacion.java
+++ src/main/java/com/servidor/servidor/controller/ControllerEducacion.java
@@ -4,6 +4,7 @@ import com.servidor.servidor.model.Educacion;
 import com.servidor.servidor.service.IEducacionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ControllerEducacion {
 
     @Autowired
