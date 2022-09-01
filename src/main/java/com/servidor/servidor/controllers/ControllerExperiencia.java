aec61d45e3c81ac8b66cf33e0fa2b77140635f56
Francisco Javier López García Mayorga <javier6282@hotmail.com>
2022-09-01 09:52:11 -0300

    [UPDATE] minor changes

--- src/main/java/com/servidor/servidor/controllers/ControllerExperiencia.java
+++ src/main/java/com/servidor/servidor/controllers/ControllerExperiencia.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin
+@CrossOrigin(origins = "https://franciscolopezgarciamayorga.web.app")
 public class ControllerExperiencia {
 
     @Autowired
